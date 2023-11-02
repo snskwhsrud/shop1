3396c1ce0ebc138cba27898bf4306e4e71cfdb28
Yang Jae Hyeok <wogur0309@naver.com>
2023-11-02 18:27:37 +0900

    11-02수업 끝

--- front/src/components/shop/orders/CartPage.jsx
+++ front/src/components/shop/orders/CartPage.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Table, Button, Spinner, Row, Col, Alert } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Table, Button, Spinner, Row, Col, Alert, Form } from "react-bootstrap";
 import Pagination from "react-js-pagination";
 import "../Pagination.css";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { BoxContext } from "../BoxContext";
 
 const CartPage = () => {
+  const { setBox } = useContext(BoxContext);
   const size = 5;
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
@@ -23,12 +26,10 @@ const CartPage = () => {
     setBooks(res.data.list);
     setTotal(res.data.total);
 
-    let sum1 = 0;
-    res.data.list.forEach((book) => {
-      sum1 += book.qnt * book.price;
-    });
-    setSum(sum1);
-
+    const res1 = await axios.get(
+      `/cart/sum?uid=${sessionStorage.getItem("uid")}`
+    );
+    setSum(res1.data.fmtsum);
     setLoading(false);
   };
 
@@ -38,6 +39,40 @@ const CartPage = () => {
 
   const onChangePage = (page) => {
     setPage(page);
+  };
+
+  const onClickDelete = (cid) => {
+    setBox({
+      show: true,
+      message: `${cid}번 장바구니 도서를 삭제하실래요?`,
+      action: async () => {
+        await axios.post("/cart/delete", { cid });
+        if (page === 1) {
+          getCart();
+        } else {
+          setPage(1);
+        }
+      },
+    });
+  };
+
+  const onClickUpdate = (cid, qnt) => {
+    setBox({
+      show: true,
+      message: `${cid}번 수량을 ${qnt}로 변경하실래요?`,
+      action: async () => {
+        await axios.post("/cart/update", { cid, qnt });
+        getCart();
+      },
+    });
+  };
+
+  const onChange = (e, cid) => {
+    setBooks(
+      books.map((book) =>
+        book.cid === cid ? { ...book, qnt: e.target.value } : book
+      )
+    );
   };
 
   if (loading)
@@ -54,14 +89,15 @@ const CartPage = () => {
           <tr>
             <td>ID</td>
             <td colSpan={2}>제목</td>
-            <td>가격</td>
+            <td className="text-end">가격</td>
             <td>수량</td>
-            <td>합계</td>
+            <td className="text-end">합계</td>
+            <td>삭제</td>
           </tr>
         </thead>
         <tbody>
           {books.map((book) => (
-            <tr key={book.bid}>
+            <tr key={book.cid}>
               <td>{book.bid}</td>
               <td>
                 <img
@@ -73,19 +109,35 @@ const CartPage = () => {
                 <div className="ellipsis">{book.title}</div>
               </td>
               <td className="text-end">{book.fmtprice}원</td>
-              <td>{book.qnt}</td>
+              <td>
+                <input
+                  onChange={(e) => onChange(e, book.cid)}
+                  value={book.qnt}
+                  size={2}
+                  className="text-end"
+                />
+                <Button
+                  onClick={() => onClickUpdate(book.cid, book.qnt)}
+                  size="sm ms-1"
+                >
+                  변경
+                </Button>
+              </td>
               <td className="text-end">{book.fmtsum}원</td>
+              <td>
+                <RiDeleteBinLine
+                  onClick={() => onClickDelete(book.cid)}
+                  className="delete"
+                />
+              </td>
             </tr>
           ))}
         </tbody>
       </Table>
       <Alert>
         <Row>
-          <Col>주문상품수량: {total}</Col>
-          <Col className="text-end">
-            총 상품금액: {sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            원
-          </Col>
+          <Col>주문상품수량: {total}권</Col>
+          <Col className="text-end">총 상품금액: {sum}원</Col>
         </Row>
       </Alert>
       {total > size && (
